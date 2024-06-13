41f1352663d839b174d07aae95f69a71d13437f1
lukashaverbeck <lukas.haverbeck@gmail.com>
2024-06-13 17:15:40 +0200

    improved setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from pathlib import Path
+
+from setuptools import setup, find_packages
 
 setup(
     name="expyro",
     version="0.0.1",
     description="A minimal library to keep track of experiments.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url="https://github.com/lukashaverbeck/expyro",
     author="Lukas Haverbeck",
     license="MIT",
-    packages=["expyro"],
-    requires=["tyro", "matplotlib"]
+    python_requires=">=3.11",
+    requires=["tyro", "matplotlib"],
+    packages=find_packages(),
+    include_package_data=True,
 )
