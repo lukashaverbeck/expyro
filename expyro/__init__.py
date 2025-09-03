a4a3215c0d70b0c61951c183e5cb29985cdcdd3e
Lukas Haverbeck <lukas.haverbeck@rwth-aachen.de>
2025-09-03 14:19:27 +0200

    added snapshots

--- expyro/__init__.py
+++ expyro/__init__.py
@@ -1,4 +1,4 @@
-from expyro._artifacts import artifact, plot, table
+from expyro._artifacts import artifact, plot, table, snapshot
 from expyro._cli import cli
 from expyro._experiment import experiment, default, defaults
 from expyro._hook import hook
@@ -8,7 +8,7 @@ __all__ = [
     "experiment",
     "default", "defaults",
     "hook",
-    "artifact", "plot", "table",
+    "artifact", "plot", "table", "snapshot",
     "postprocess", "rename", "move",
     "cli"
 ]
